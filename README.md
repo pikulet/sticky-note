9707d194fa5dcb755700493330b5aa749d9b72d2
pikulet <jycyeo@yahoo.com.sg>
2019-12-24 22:04:00 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # sticky-note
-a sticky note application
+A sticky note application on the web
+
+Deployed with surge: [pin.surge.sh](pin.surge.sh)
+
+![example](example.png)
+
+## Improvements
+- Resizing notes
+- Changing font size and font family
+- A nicer background image helps
+
+## Thank you
+
+[Tyler Feliz](https://www.admixweb.com/create-a-posticks-sticky-notes-app-with-html5-css3-and-jquery/) wrote an awesome guide. I simplified a lot of the JS to make it more understandable.
---
+++ js/main.js
@@ -0,0 +1,77 @@
+draggableNote = {
+    cancel: '.editable',
+    "zIndex": 3000,
+    "stack" : '.note'
+};
+
+$(document).ready(function () {
+ 
+    addNotesFromStorage();
+
+    $(".note").draggable(draggableNote);
+ 
+    //Remove Note
+    $('span.delete').click(deleteNote);
+ 
+    //Create Note
+    $('#btn-addNote').click(addNote);
+
+    //Save all the notes when the user leaves the page
+    window.onbeforeunload = saveNotesToStorage;    
+});
+
+function addNote() {
+    var emptyNote = createNote(100, 100, "");
+    $('#board').append(emptyNote);
+    $(".note").draggable(draggableNote);
+}
+
+function deleteNote() {
+    $(this).closest('.note').fadeOut('slow', 
+        function () {
+            $(this).remove();
+        }
+    );
+}
+
+function createNote(left, top, text) {
+    note =  ''
+        +   '<div class="note" '
+        // position of note
+        +   'style="left:' + left + 'px; '
+        +   'top:' + top + 'px" >'
+        // note content
+        +   '<div class="toolbar">'
+        +   '<span class="delete"> &times; </span>' 
+        +   '</div>'
+        +   '<div contenteditable="true" class="editable">'
+        +   text
+        +   '</div>';
+    return note;
+}
+
+function addNotesFromStorage() {
+    numNotes = window.localStorage.length;
+    if (numNotes > 0) {
+        for (var i = 0; i < numNotes; i++) {
+            var id = window.localStorage.key(i);
+            noteObject = JSON.parse(window.localStorage.getItem(id));
+            note = createNote(noteObject.left, noteObject.top, noteObject.text);
+            $('#board').append(note);
+        }
+    }
+}
+
+function saveNotesToStorage() {
+    window.localStorage.clear();
+    $('.note').each(function () {
+        noteObject = {
+                top     :   parseInt($(this).position().top),
+                left    :   parseInt($(this).position().left),
+                text    :    $(this).children('.editable').text(),
+                id      :   window.localStorage.length
+        }
+
+        window.localStorage.setItem(noteObject.id, JSON.stringify(noteObject));
+    });
+}
